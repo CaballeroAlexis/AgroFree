3d8a151a4a6bdd3fb7a53fbf482de2058c2020be
Alexis <alexis.wolfy@gmail.com>
2019-10-17 00:57:11 -0300

    Limpiando

--- src/app/table-example/table-example.component.ts
+++ src/app/table-example/table-example.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 import * as fs from 'fs';
-import * as path from 'path';
+//import * as path from 'path';
 
 // tslint:disable-next-line:no-implicit-dependencies
-import { Menu, MenuItemConstructorOptions, OpenDialogOptions, remote } from 'electron';
 
 import { Hero } from '../model/hero';
 import { Settings } from '../model/settings';
@@ -21,10 +20,6 @@ export class TableExampleComponent {
 
       if (fs.existsSync(Settings.dbPath)) {
           this.openDb(Settings.dbPath);
-      } else if (Settings.hasFixedDbLocation) {
-          this.createDb(Settings.dbPath);
-      } else {
-          this.createDb();
       }
   }
 
@@ -45,46 +40,6 @@ export class TableExampleComponent {
           });
   }
 
-  public async createDb(filename?: string) {
-      if (!filename) {
-          const options: OpenDialogOptions = {
-              title: 'Create file',
-              defaultPath: remote.app.getPath('documents'),
-              filters: [
-                  {
-                      name: 'Database',
-                      extensions: ['db'],
-                  },
-              ],
-          };
-          filename = await remote.dialog.showSaveDialogSync(remote.getCurrentWindow(), options);
-      }
-
-      if (!filename) {
-          return;
-      }
-
-      TheDb.createDb(filename)
-          .then((dbPath) => {
-              if (!Settings.hasFixedDbLocation) {
-                  Settings.dbPath = dbPath;
-                  Settings.write();
-              }
-          })
-          .then(() => {
-              this.getHeroes();
-          })
-          .catch((reason) => {
-              console.log(reason);
-          });
-  }
-
-  public onRestoreDb() {
-      TheDb.importJson(path.join(Settings.dbFolder, 'database.init.json'), false)
-          .then(() => {
-              this.getHeroes();
-          });
-  }
 
   public getHeroes() {
       Hero.getAll()
@@ -94,24 +49,4 @@ export class TableExampleComponent {
           });
   }
 
-  public onMenu(hero: Hero) {
-      const menu = this.initMenu(hero);
-      menu.popup({});
-  }
-
-  private deleteHero(hero: Hero) {
-      hero.delete();
-      this.getHeroes();
-  }
-
-  private initMenu(hero: Hero): Menu {
-      const template: MenuItemConstructorOptions[] = [
-          {
-              label: `Delete ${hero.name}`,
-              click: () => this.deleteHero(hero),
-          },
-      ];
-
-      return remote.Menu.buildFromTemplate(template);
-  }
 }
